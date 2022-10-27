aba2406470c93783021419e14a846b97a4bd516d
alan7566 <83546356+alan7566@users.noreply.github.com>
2022-10-27 10:57:54 -0500

    Carrito de compras

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 const nav_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
+const desktop_menu_button = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 nav_email.addEventListener('click', toggleDesktopEmail); 
+desktop_menu_button.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarrito);
 
 function toggleDesktopEmail() {
+  const isAsideActive = !aside.classList.contains('inactive');
+
+  if (isAsideActive) {
+    aside.classList.toggle('inactive');
+  }
+
   desktop_menu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+  const isAsideActive = !aside.classList.contains('inactive');
+  if (isAsideActive) {
+    aside.classList.toggle('inactive');
+  }
+
+
+    mobile_menu.classList.toggle('inactive');
+  }
+
+function toggleCarrito() {
+  const isMobileMenuActive = !mobile_menu.classList.contains('inactive');
+  const isdesktopEmailActive = !desktop_menu.classList.contains('inactive');
+
+    /* aside.classList.toggle('inactive'); */
+    
+    if (isMobileMenuActive) {
+      mobile_menu.classList.toggle('inactive');
+    }
+    if (isdesktopEmailActive) {
+      desktop_menu.classList.toggle('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+    
+  }
